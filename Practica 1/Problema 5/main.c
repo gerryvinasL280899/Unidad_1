9b0e04e19783025e688c0e06c8780c4fdb5b54d2
gerryvinasL280899 <32582557+gerryvinasL280899@users.noreply.github.com>
2017-10-22 18:52:32 -0500

    .

--- Practica 1/Problema 5/main.c
+++ Practica 1/Problema 5/main.c
@@ -4,19 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	float G;
-	char L[]={};
-	printf("Introdusca los grados y la inicial del nombre \n");
-	scanf("%f",&G);
+	float G,g;
+	char L;
+	printf("Introdusca la inicial de la medida \n");
 	scanf("%c",&L);
-	if ("%c",L==c) {
-		G=(9*G)/5+32;
-		L=f;
+	printf("Introdusca los grados \n");
+	scanf("%f",&g);
+	if (L=='c') {
+		G=1.8*g+32;
+		printf("La conversion es de %.2f Farenheit",G);
 	}
 	else {
-		G=(G-32)*(5/9);
-		L=c;
+		
+		if(L=='f'){
+		
+		G=(g-32)*5/9; 
+		printf("La conversion es de %.2f Celsius",G);
 	}
-	printf("La conversion es de %f %c",G,L);
+}
 	return 0;
 }

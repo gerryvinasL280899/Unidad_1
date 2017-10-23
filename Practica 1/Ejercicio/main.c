f5891d2031a412d5d31ef54c46f9c2ef826a3c38
gerryvinasL280899 <32582557+gerryvinasL280899@users.noreply.github.com>
2017-10-23 17:01:43 -0500

    .

--- Practica 1/Ejercicio/main.c
+++ Practica 1/Ejercicio/main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	float d,h,p,D,P,a,V;
+	float d,h,D,P,a,V;
 	int A;
 	const float g=9.81;
 	const float pi=3.1416;
@@ -12,12 +12,12 @@ int main(int argc, char *argv[]) {
 	printf("Introducir el diametro del pozo \n");
 	scanf("%f",&d);
 	printf("Introducir la presion hidrostatica \n");
-	scanf("%f",&p);
+	scanf("%f",&P);
 	printf("Seleccione que liquido se utilizo \n");
 	printf("Opcion 1: Agua \n");
 	printf("Opcion 2: Diesel \n");
 	scanf("%i",&A);
-	(A==1)?(D=1000*10):(D=820*10);
+	(A==1)?(D=1000*10*P):(D=820*10*P);
 	h=P/(D*g);
 	a=(d/2)*(d/2)*pi;
 	V=a*h;
